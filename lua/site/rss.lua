7b10df2119f8a8f27fc8a5641fc5ba1bf578a31c
Oleksandr Smirnov <olexsmir@gmail.com>
2025-09-29 19:56:56 +0300

    escape html inside of atom feed

--- lua/site/rss.lua
+++ lua/site/rss.lua
@@ -3,6 +3,19 @@ local date = require("site.date").date
 local a = require "site.html.attribute"
 local rss = {}
 
+function rss.escape_html(html)
+  local map = {
+    ["&"] = "&amp;",
+    ["<"] = "&lt;",
+    [">"] = "&gt;",
+    ['"'] = "&quot;",
+    ["'"] = "&#39;",
+  }
+  return (html:gsub("[&<>\"']", function(c)
+    return map[c]
+  end))
+end
+
 ---@param config {feed_url:string, home_url:string, name:string, email:string}
 ---@param posts site.Post[]
 ---@return string
@@ -16,7 +29,7 @@ function rss.rss(posts, config)
         h.el("link", { a.href(config.home_url .. "/" .. post.meta.slug) }, {}),
         h.el("id", {}, { h.text(config.home_url .. "/" .. post.meta.slug) }),
         h.el("updated", {}, { h.text(date(post.meta.date)) }),
-        h.el("content", { a.attr("type", "html") }, { h.raw(post.content) }), -- TODO: render me
+        h.el("content", { a.attr("type", "html") }, { h.raw(rss.escape_html(post.content)) }),
       })
     end)
     :totable()
@@ -25,8 +38,8 @@ function rss.rss(posts, config)
     .. h.render(h.el("feed", { a.attr("xmlns", "http://www.w3.org/2005/Atom") }, {
       h.el("title", {}, { h.text(config.name) }),
       h.el("subtitle", {}, { h.text "olexsmir's blog feed" }),
-      h.el("id", {}, { h.text(config.home_url) }),
-      h.el("link", { a.href(config.home_url) }, {}),
+      h.el("id", {}, { h.text(config.home_url .. "/") }),
+      h.el("link", { a.href(config.home_url), a.attr("rel", "alternate") }, {}),
       h.el("link", {
         a.attr("type", "application/atom+xml"),
         a.href(config.feed_url),
