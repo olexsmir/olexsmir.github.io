c663bf387b1d537ac8d8b6c11eb334005daa4d99
Oleksandr Smirnov <olexsmir@gmail.com>
2025-11-06 18:27:55 +0200

    feat: write cname

--- lua/blog/config.lua
+++ lua/blog/config.lua
@@ -4,6 +4,7 @@ config.name = "olexsmir"
 config.title = "olexsmir's blog"
 config.email = "olexsmir@cock.li"
 config.url = "https://olexsmir.github.io"
+config.cname = "olexsmir.xyz"
 config.feed = {
   subtitle = "olexsmir's blog feed",
   url = config.url .. "/feed.xml",
